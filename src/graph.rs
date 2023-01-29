33a9be7017fea350da58ed36939fb03bcf113155
Michele Maione <mikymaione@hotmail.it>
2023-01-29 18:50:09 +0100

    opt

--- src/graph.rs
+++ src/graph.rs
@@ -4,9 +4,7 @@ MIT License
 Copyright (c) 2023 Michele Maione
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
@@ -36,10 +34,10 @@ impl Graph {
     }
 
     fn bleach(&mut self) {
-        self.vertices.iter().for_each(|v| {
+        for v in &self.vertices {
             self.color.insert(*v, Color::White);
             self.distance.insert(*v, 0);
-        });
+        }
     }
 
     fn add_node(&mut self, i: i32) {
@@ -49,13 +47,16 @@ impl Graph {
     }
 
     pub fn add_edge(&mut self, from: i32, to: i32) {
-        if self.adjacent.contains_key(&from) {
-            self.add_node(to);
-            self.adjacent.get_mut(&from).unwrap().insert(to);
-        } else {
-            self.add_node(to);
-            self.adjacent.insert(from, HashSet::from([to]));
-        }
+        self.add_node(to);
+
+        match self.adjacent.get_mut(&from) {
+            Some(l) =>
+                l.insert(to),
+
+            None =>
+                self.adjacent.insert(from, HashSet::from([to]))
+                    .is_some(),
+        };
 
         if !self.adjacent.contains_key(&to) {
             self.adjacent.insert(to, HashSet::new());
@@ -65,16 +66,13 @@ impl Graph {
     pub fn bfs(&mut self, s: i32) {
         self.bleach();
 
-        let mut q = VecDeque::new();
-        q.push_back(s); // enqueue
+        let mut q = VecDeque::from([s]);
 
         self.color.insert(s, Color::Gray);
         self.distance.insert(s, 0);
 
-        while !q.is_empty() {
-            let v = q.pop_front().unwrap(); // dequeue
-
-            self.adjacent[&v].iter().for_each(|w|
+        while let Some(v) = q.pop_front() {
+            for w in &self.adjacent[&v] {
                 match self.color[&w] {
                     Color::White => {
                         self.color.insert(*w, Color::Gray);
@@ -86,7 +84,7 @@ impl Graph {
 
                     _ => {}
                 }
-            );
+            }
 
             self.color.insert(v, Color::Black);
         }
